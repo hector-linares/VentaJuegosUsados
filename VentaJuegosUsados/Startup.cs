1317efeb812d3ea47908b8ce3019d99dcaa073d2
Pablo Rioseco <Pablo Rioseco@DESKTOP-O3A9HGE>
2016-11-08 19:46:04 -0300

    Agregar archivos de proyecto.

---
+++ VentaJuegosUsados/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(VentaJuegosUsados.Startup))]
+namespace VentaJuegosUsados
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
